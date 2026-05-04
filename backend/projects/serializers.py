143a63f2c982a844792f6e60021753f571848b60
unknown <vutikurishanmukha@gmail.com>
2026-05-04 11:38:14 +0530

    fix: allow ProjectCreateUpdateSerializer to accept and process team_members during project creation and update

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from accounts.models import User
 from accounts.serializers import UserListSerializer
 
 from .models import Project
@@ -62,9 +63,13 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
 class ProjectCreateUpdateSerializer(serializers.ModelSerializer):
     """Serializer for creating and updating projects."""
 
+    team_members = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), many=True, required=False
+    )
+
     class Meta:
         model = Project
-        fields = ["id", "name", "description"]
+        fields = ["id", "name", "description", "team_members"]
         read_only_fields = ["id"]
 
     def validate_name(self, value):
@@ -73,6 +78,31 @@ class ProjectCreateUpdateSerializer(serializers.ModelSerializer):
         if not value:
             raise serializers.ValidationError("Project name cannot be empty.")
         return value
+
+    def update(self, instance, validated_data):
+        team_members = validated_data.pop("team_members", None)
+        instance = super().update(instance, validated_data)
+
+        if team_members is not None:
+            from .services import remove_team_member
+            current_members = set(instance.team_members.all())
+            new_members = set(team_members)
+
+            # Remove members not in the new list (this auto-unassigns tasks via the service)
+            for user in current_members - new_members:
+                remove_team_member(instance, user.id)
+
+            # Add remaining members
+            instance.team_members.set(team_members)
+
+        return instance
+
+    def create(self, validated_data):
+        team_members = validated_data.pop("team_members", [])
+        instance = super().create(validated_data)
+        if team_members:
+            instance.team_members.set(team_members)
+        return instance
 
 
 class TeamMemberSerializer(serializers.Serializer):
